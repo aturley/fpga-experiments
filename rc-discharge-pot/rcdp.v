67ca71ee1da485aa0753b03806a57a7af7aaa912
Andrew Turley <aturley@acm.org>
2022-05-02 17:55:16 -0500

    Reorganize discharge pot code

--- rc-discharge-pot/rcdp.v
+++ rc-discharge-pot/rcdp.v
@@ -14,7 +14,7 @@ module top(
    parameter      FINISHED = 2;
    parameter      DISPLAY = 3;
 
-   parameter      CHARGING_TICKS = 240000;
+   parameter      CHARGING_TICKS = 12000;
 
    reg [32:0]            counter;
    reg [32:0]            accumulator;
@@ -28,46 +28,110 @@ module top(
    assign resetn = &resetn_counter;
    assign green = in;
    assign out = out_hi ? 1'b1 : 1'bz;
+   assign {disp0, disp1, disp2, disp3} = counter[19:16];
 
    always @ (posedge clki)
      begin
         if (!resetn)
           begin
              resetn_counter <= resetn_counter + 1;
-             state <= CHARGING;
-             accumulator <= 32'd0;
-             counter <= 32'd0;
-             out_hi <= 1;
-          end
-        else
-          begin
-             case (state)
-               CHARGING:
-                 begin
-                    out_hi <= 1;
-                    state <= (accumulator > CHARGING_TICKS) ? MEASURING : CHARGING;
-                    accumulator <= (accumulator > CHARGING_TICKS) ? 0 : accumulator + 1;
-                 end
-               MEASURING:
-                 begin
-                    out_hi <= 0;
-                    accumulator <= accumulator + 1;
-                    state <= (in) ? MEASURING : FINISHED;
-                 end
-               FINISHED:
-                 begin
-                    out_hi <= 0;
-                    counter <= accumulator;
-                    state <= DISPLAY;
-                 end
-               DISPLAY:
-                 begin
-                    out_hi <= 0;
-                    accumulator <= 32'd0;
-                    {disp0, disp1, disp2, disp3} <= counter[19:16];
-                    state <= CHARGING;
-                 end
-             endcase // case (state)
           end
      end
+
+   // state machine
+   always @ (posedge clki)
+     if (!resetn)
+       begin
+          state <= CHARGING;
+       end
+     else
+       begin
+          case (state)
+            CHARGING:
+              begin
+                 state <= (accumulator > CHARGING_TICKS) ? MEASURING : CHARGING;
+              end
+            MEASURING:
+              begin
+                 state <= (in) ? MEASURING : FINISHED;
+              end
+            FINISHED:
+              begin
+                 state <= DISPLAY;
+              end
+            DISPLAY:
+              begin
+                 state <= CHARGING;
+              end
+          endcase // case (state)
+       end
+
+   // accumulator
+   always @ (posedge clki)
+     if (!resetn)
+       begin
+          accumulator = 32'd0;
+       end
+     else
+       begin
+          case (state)
+            CHARGING:
+              begin
+                 accumulator <= (accumulator > CHARGING_TICKS) ? 0 : accumulator + 1;
+              end
+            MEASURING:
+              begin
+                 accumulator <= accumulator + 1;
+              end
+            DISPLAY:
+              begin
+                 accumulator <= 32'd0;
+              end
+          endcase // case (state)
+     end
+
+   // counter
+   always @ (posedge clki)
+     if (!resetn)
+       begin
+          counter <= 32'd0;
+       end
+     else
+       begin
+          case (state)
+            FINISHED:
+              begin
+                 counter <= accumulator;                 
+              end
+          endcase // case (state)
+     end
+
+   // out_hi
+   always @ (posedge clki)
+     if (!resetn)
+       begin
+          out_hi <= 1;
+       end
+     else
+       begin
+          case (state)
+            CHARGING:
+              begin
+                 out_hi <= 1;
+              end
+            MEASURING:
+              begin
+                 out_hi <= 0;
+              end
+            FINISHED:
+              begin
+                 out_hi <= 0;
+              end
+            DISPLAY:
+              begin
+                 out_hi <= 0;
+              end
+          endcase // case (state)
+       end
+   
 endmodule
